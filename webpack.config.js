d4850528918e402e6d8c207f0dc28171c5c15a60
Christopher Lowenthal <clowenhg@gmail.com>
2018-03-16 03:37:40 -0400

    Pushing current code

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    filename: "ody.js",
+    path: __dirname + "/dist"
+  },
+
+  devtool: "source-map",
+
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ]
+  },
+
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
+  }
+}
